473aa182f31266aa4f603db7e9168fd6bcafeb0e
Daniel Falbel <dfalbel@gmail.com>
2018-04-28 08:18:39 -0300

    adds protobuf3 installation script

---
+++ scripts/protobuf-installer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl -OL https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-cpp-3.5.1.zip
+unzip protobuf-cpp-3.5.1.zip -d proto
+cd proto/protobuf-3.5.1/
+./configure
+make
+sudo make install
+sudo ldconfig

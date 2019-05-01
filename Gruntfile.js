f2f91effe97b92a0063d0fd8dc05416cd8309340
Gábor Csárdi <csardi.gabor@gmail.com>
2019-05-01 13:28:39 +0100

    Run validation of sysreqs from grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,10 @@
 
 module.exports = function (grunt) {
 
+  // -----------------------------------------------------------
+
   grunt.loadNpmTasks('grunt-jsonlint');
+  grunt.loadNpmTasks('grunt-tv4');
 
   grunt.initConfig({
 
@@ -10,14 +13,22 @@ module.exports = function (grunt) {
       sample: {
         src: [ 'cran/*.json', 'platforms/*.json', 'sysreqs/*.json' ]
       }
+    },
+
+    tv4: {
+        options: {
+            root: grunt.file.readJSON('schema-sysreq.json')
+        },
+        myTarget: {
+            src: [ 'sysreqs/*.json' ]
+        }
     }
 
   });
 
   // Default task.
-  grunt.registerTask('default', 'jsonlint');
+  grunt.registerTask('default', [ 'jsonlint', 'tv4' ]);
 
   // Travis CI task.
-  grunt.registerTask('travis', 'jsonlint');
-
+  grunt.registerTask('travis', [ 'jsonlint', 'tv4' ]);
 };
